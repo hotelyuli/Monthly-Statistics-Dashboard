51bdaa64d19e06206c5a158e3178c79a75d38036
hotelyuli <yusreijamil@hotelyuli.com>
2026-05-20 11:12:03 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './hotel_yuli_2026_dashboard.jsx'
+import App from './hotel_yuli_dashboard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
